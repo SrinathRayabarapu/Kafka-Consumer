94e8928c242baaae2e7c811e92c010b76d669e89
Srinath Rayabarapu <srinath.rayabarapu@tesco.com>
2022-08-03 15:35:31 +0530

    Consuming data from a topic

--- src/main/java/com/kafka/consumer/HelloWorldKafkaConsumer.java
+++ src/main/java/com/kafka/consumer/HelloWorldKafkaConsumer.java
@@ -4,8 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Service
 @Slf4j
+@Service
 public class HelloWorldKafkaConsumer {
 
     @KafkaListener(topics = "t_test_topic")
